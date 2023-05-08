f3346a5e8ad2d1e01ccec23587cf759c562fcab8
Theanh Nguyen <85526287+PhantomSs108@users.noreply.github.com>
2023-05-07 23:54:29 -0700

    completed viewSeating method

--- main.py
+++ main.py
@@ -10,17 +10,46 @@ def createSeats():
             row.append('.')
         seating.append(row)
 
-def printSeats():
-    for r in range(n_row):
-        print(r+1, end="\t")
-        for c in range(n_col):
-            print(seating[r][c], end=" ")
-        print()
-
 def printLines():
     for i in range(80):
         print('-', end ='')
     print()
+
+def viewSeating():
+    printLines()
+    print("Seating")
+    printLines()
+
+    print(" ", end = "\t")
+    for i in range(n_col):
+        print(chr(i+65), end = " ")
+    print(" ", end = "\t")
+    print("type", end = "\t")
+    print("price", end = "\n")
+
+    print(" ", end = "\t")
+    for i in range (n_col * 2):
+        print("-", end = "")
+    print(" ", end = "\t")
+    print("-----", end = "\t")
+    print("-----", end = "\n")
+
+    for r in range(n_row):
+        print(r+1, end="\t")
+        for c in range(n_col):
+            print(seating[r][c], end=" ")
+        print(" ", end = "\t")
+        if (r < 5):
+            print("front", end = "\t")
+            print("$80", end = "\n")
+        elif (r < 11):
+            print("middle", end = "\t")
+            print("$50", end = "\n")
+
+        elif(r < 21):
+            print("back", end = "\t")
+            print("$80", end = "\n")
+
 
 def printMenu():
     printLines()
@@ -32,4 +61,10 @@ def printMenu():
     print('[d] display all the purchases made and the total amount of income')
     print('[q] to quit')
 
+createSeats()
 printMenu()
+command = input("Enter a command: ")
+match command:
+    case 'v':   
+        viewSeating()
+
