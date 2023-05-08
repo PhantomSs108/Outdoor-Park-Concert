ffac2b0adf09846c2be084d8311ca1a8d7076ca4
Theanh Nguyen <85526287+PhantomSs108@users.noreply.github.com>
2023-05-08 01:17:34 -0700

    added receipt print with formatting and started on buy method

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def viewSeating():
     print("-----", end = "\n")
 
     for r in range(n_row):
-        print(r+1, end="\t")
+        print(r, end="\t")
         for c in range(n_col):
             print(seating[r][c], end=" ")
         print(" ", end = "\t")
@@ -45,10 +45,9 @@ def viewSeating():
         elif (r < 11):
             print("middle", end = "\t")
             print("$50", end = "\n")
-
-        elif(r < 21):
+        else:
             print("back", end = "\t")
-            print("$80", end = "\n")
+            print("$25", end = "\n")
 
 
 def printMenu():
@@ -61,10 +60,80 @@ def printMenu():
     print('[d] display all the purchases made and the total amount of income')
     print('[q] to quit')
 
+def buyTickets():
+    numberOfTickets = int(input("Number of seats to buy: "))
+    startingSeat = input("Starting seat (ex. 3D): ")
+    row = int(startingSeat[0])
+    
+    #set price and seat price
+    if (row < 5):
+        price = 80
+        seatType = "front"
+    elif(row < 11):
+        price = 50
+        seatType = "middle"
+    else:
+        price = 25
+        seatType = "back"
+    
+    #check if seats are available
+    available = True
+    for i in range(numberOfTickets):
+        if (seating[row][ord(startingSeat[1]) - 65 + i] != '.'):
+            available = False
+    if (available == False):
+        print("The seats you chose are not available for purchase. Please try again.")
+        return
+    
+    print(numberOfTickets, " seats starting at (", startingSeat, ") are available for purchase", end = "\n")
+    name = input("Enter your name: ")
+    email = input("Enter your email address: ")
+
+    #calculate costs and print receipt
+    printLines()
+    print("Receipt")
+    printLines()
+    print("Name:", "\t", "\t", end = "\t")
+    print(name)
+    print("Email:", "\t", "\t", end = "\t")
+    print(email)
+    print("Number of Tickets:", end = "\t")
+    print(numberOfTickets)
+    print("Seat Type:", "\t", end = "\t")
+    print(seatType)
+    print("Seats: ", "\t", end = "\t")
+    for i in range (numberOfTickets):
+        print(startingSeat[0], chr(ord(startingSeat[1]) + i), sep = '', end = " ")
+    print()
+    print("Ticket Cost:", "\t", end = "\t")
+    ticketCosts = price * numberOfTickets
+    print(f"${ticketCosts:.2f}")
+    print("Mask Fee: ", "\t", end = "\t")
+    maskFee = numberOfTickets * 5
+    print(f"${maskFee:.2f}")
+    print("Sub-total: ", "\t", end = "\t")
+    subtotal = price * numberOfTickets + numberOfTickets * 5
+    print(f"${subtotal:.2f}")
+    tax = subtotal * 0.0725
+    total = subtotal + tax
+    print("Tax:", "\t", "\t", end = "\t")
+    print(f"${tax:.2f}")
+    printLines()
+    print("Total:", "\t", "\t", end = "\t")
+    print(total)
+    printLines()
+
+
+
 createSeats()
-printMenu()
-command = input("Enter a command: ")
-match command:
-    case 'v':   
-        viewSeating()
+command = ""
+
+while (command != 'q'):
+    printMenu()
+    command = input("Enter a command: ")
+    match command:
+        case 'b': 
+            buyTickets()
+        case 'v':   
+            viewSeating()
 
